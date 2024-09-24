6167aed61618091447fd0078cba1a41df7a5b4fd
firashacker <firastom0@gmail.com>
2024-09-24 23:32:18 +0300

    sat lsp for ts js tsx [typescript-language-server],sat html autoclose with treesitter and nvim-ts-autotag,sat autoformating with prettier

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,4 +23,34 @@ return {
       },
     },
   },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+    },
+    opts = {
+      enable_close = true, -- Auto close tags
+      enable_rename = true, -- Auto rename pairs of tags
+      enable_close_on_slash = true, -- Auto close on trailing </
+    },
+    config = function()
+      require("nvim-ts-autotag").setup() -- can pass opts here like this { opts }
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function()
+      opts = require "configs.treesitter"
+      opts.ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "tsx",
+      }
+      return opts
+    end,
+  },
 }
