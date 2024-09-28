6d70f3b8d64e926bde172d52ae0e64b41c1ed295
firashacker <firastom0@gmail.com>
2024-09-28 23:43:39 +0300

    transparency set

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,18 +1,18 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onenord",
+  theme = "onenord",
   transparency = true,
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
-M.plugins = require("plugins.init")
+M.plugins = require "plugins.init"
 
 return M
