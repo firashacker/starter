4500e1580c0c7f712731df8600cbef0b6a2ae71f
firashacker <firastom0@gmail.com>
2024-10-10 03:14:48 +0300

    mason install all issue https://github.com/williamboman/mason.nvim/discussions/1795 work around by runing function in init script

--- init.lua
+++ init.lua
@@ -31,6 +31,7 @@ dofile(vim.g.base46_cache .. "statusline")
 
 require "options"
 require "nvchad.autocmds"
+-- require('nvchad.mason').installall()
 
 vim.schedule(function()
   require "mappings"
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,10 +4,10 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", "<C-h>", "<cmd> TmuxNavigateLeft <cr>" )
-map("n", "<C-l>", "<cmd> TmuxNavigateRight <cr>" )
-map("n", "<C-j>", "<cmd> TmuxNavigateDown <cr>" )
-map("n", "<C-k>", "<cmd> TmuxNavigateUp <cr>" )
+map("n", "<C-h>", "<cmd> TmuxNavigateLeft <cr>")
+map("n", "<C-l>", "<cmd> TmuxNavigateRight <cr>")
+map("n", "<C-j>", "<cmd> TmuxNavigateDown <cr>")
+map("n", "<C-k>", "<cmd> TmuxNavigateUp <cr>")
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
